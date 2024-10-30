89e4527f50afe4e0e7b0a0f61abcf405dc7ddab2
gdvr <chivodeoriente@gmail.com>
2024-10-30 00:20:28 -0600

    generate the files to using into the training stage

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,32 +1,50 @@
 # src/preprocess.py
 import pandas as pd
 import sys
+import yaml
+
+from utils.common import categorizeColumns, detectInvalidValues, handlingEmptyValues, splitValuesForModel
 
 def preprocess(input_file, output_file, features, target):
-    # Cargar el dataset
     df = pd.read_csv(input_file)
-
-    # Eliminar filas con valores nulos
     df = df.dropna()
-
-    # Seleccionar solo las columnas necesarias
     columns = features + [target]
     df = df[columns]
 
-    # Guardar el dataset limpio
+    continuas, discretas, categoricas = categorizeColumns(df)
+    detectInvalidValues( df[columns])
+    handlingEmptyValues(df[columns].copy(),continuas + discretas)
+
+    X = df[features]
+    y = df[target]
+    X_train, y_train,X_test,y_test,X_val, y_val = splitValuesForModel(X,y,params['train']['TEST_SIZE'],params['train']['VALIDATE_SIZE'],params['train']['RANDOM_STATE'])
+    X_train[target] = y_train
+    X_test[target] = y_test
+    X_val[target] = y_val
+
+    X_train.to_csv("data/X_train.csv", index=False)
+    X_test.to_csv("data/X_test.csv", index=False)
+    X_val.to_csv("data/X_val.csv", index=False)
+
     df.to_csv(output_file, index=False)
     print(f"Preprocesamiento completado. Datos guardados en {output_file}")
 
+    params['continuas'] = continuas
+    params['discretas'] = discretas
+    params['categoricas'] = categoricas
+
+    with open("params.yaml", "w") as f:
+        yaml.dump(params, f, default_flow_style=False, sort_keys=False)
+
 if __name__ == "__main__":
-    # Argumentos: archivo de entrada, archivo de salida, características y objetivo
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     params_file = sys.argv[3]
 
-    # Leer parámetros desde params.yaml
-    import yaml
     with open(params_file) as f:
         params = yaml.safe_load(f)
+    
+    print(params)
 
     features = params['preprocessing']['features']
     target = params['preprocessing']['target']
