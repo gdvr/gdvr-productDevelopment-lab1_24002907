d3493c3832bcdbe61f2a620bff9b7cab3ef78fa4
gdvr <chivodeoriente@gmail.com>
2024-11-01 10:53:52 -0600

    last changes

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,6 @@
 # src/preprocess.py
 import pandas as pd
 import sys
-import yaml
 
 from utils.common import categorizeColumns,  detectInvalidValues, handlingEmptyValues
 
@@ -15,10 +14,6 @@ def preprocess(input_file, output_file, features, target):
     detectInvalidValues(df[columns])
     handlingEmptyValues(df[columns].copy(),continuas + discretas)
 
-
-    df.to_csv(output_file, index=False)
-    print(f"Preprocesamiento completado. Datos guardados en {output_file}")
-
     params['continuas'] = continuas
     params['discretas'] = discretas
     params['categoricas'] = categoricas
@@ -26,11 +21,16 @@ def preprocess(input_file, output_file, features, target):
     with open("params.yaml", "w") as f:
         yaml.dump(params, f, default_flow_style=False, sort_keys=False)
 
+    df.to_csv(output_file, index=False)
+    print(f"Preprocesamiento completado. Datos guardados en {output_file}")
+
 if __name__ == "__main__":
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     params_file = sys.argv[3]
 
+    
+    import yaml
     with open(params_file) as f:
         params = yaml.safe_load(f)
     
